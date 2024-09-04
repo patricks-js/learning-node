ba696cf9a12a3eaac19fa2b42c4410f03fed47f7
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-09-03 22:10:45 -0300

    make create user route

--- 03-solid/src/config/app.ts
+++ 03-solid/src/config/app.ts
@@ -1,5 +1,28 @@
 import fastify from "fastify";
+import z from "zod";
+import { prisma } from "./prisma";
 
-const app = fastify();
+export const app = fastify();
 
-export { app };
+app.post("/users", async (request, reply) => {
+  const registerBody = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string(),
+  });
+
+  const { name, email, password } = registerBody.parse(request.body);
+
+  const { id } = await prisma.user.create({
+    data: {
+      name,
+      email,
+      passwordHash: password,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return reply.status(201).send({ userId: id });
+});
